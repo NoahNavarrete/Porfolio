2a92815c6d60f77ec3039d00d50f2eb891e2002b
Noah <noenavarrete71@outlook.com>
2023-03-14 12:48:05 -0300

    se agrega data

---
+++ src/Components/Main/SectionTecnologiesData.jsx
@@ -0,0 +1,48 @@
+
+export const Data =[
+    {
+        id: 1,
+        name: "Html",
+        img: "https://th.bing.com/th/id/R.a6a70fb2455d4eb26a7e167e678f40b5?rik=KYyIadxz7MQdtw&pid=ImgRaw&r=0",
+        desc: {
+            desc1: "Etiquetas",
+            desc2: "opcion 2",
+            desc3: "opcion 3"
+        }
+
+    },
+    {
+        id: 1,
+        name: "Html",
+        img: "#",
+        desc: {
+            desc1: "Etiquetas",
+            desc2: "opcion 2",
+            desc3: "opcion 3"
+        }
+
+    },
+    {
+        id: 1,
+        name: "Html",
+        img: "#",
+        desc: {
+            desc1: "Etiquetas",
+            desc2: "opcion 2",
+            desc3: "opcion 3"
+        }
+
+    },
+    {
+        id: 1,
+        name: "Html",
+        img: "#",
+        desc: {
+            desc1: "Etiquetas",
+            desc2: "opcion 2",
+            desc3: "opcion 3"
+        }
+
+    }
+
+]
