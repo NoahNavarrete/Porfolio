60f266d64393ce10d6e6876e92a8ac76175e65d9
Noah <noenavarrete71@outlook.com>
2023-03-14 12:48:42 -0300

    se agrega componente tecnologies

--- src/Components/Main/SectionTecnologies.jsx
+++ src/Components/Main/SectionTecnologies.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Data } from "./SectionTecnologiesData";
 
 function SectionTecnologies(){
     return(
-        <section className="h-screen bg-slate-300 pt-20">
+        <section className="h-screen bg-slate-300 pt-20 flex ">{Data.map(data=> 
             <div className="h-96 w-60 p-4  bg-purple-200 flex flex-col">
-                <div className="bg-red-200 h-2/3">imagen</div>
-                <div className="bg-slate-200 flex justify-center">titulo</div>
+            <div className="bg-red-200 h-2/3">
+                <img src={data.img}></img>
             </div>
+            <div className="bg-slate-200 flex justify-center">
+            <h2>{data.name}</h2>
+               
+            </div>
+        </div>
+        )}
+          
         </section>
         )
 }
