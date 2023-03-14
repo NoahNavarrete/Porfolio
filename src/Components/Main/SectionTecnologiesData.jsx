2700aae8d93162bb6fdf015510c2be8a56ff7448
Noah <noenavarrete71@outlook.com>
2023-03-14 14:59:59 -0300

    se modifica el alto

--- src/Components/Main/SectionTecnologiesData.jsx
+++ src/Components/Main/SectionTecnologiesData.jsx
@@ -1,9 +1,15 @@
-
+import html from "./Img/Html.png"
+import css from "./Img/Css.png"
+import js from "./Img/JS.png"
+import react from "./Img/React.png"
+import tailwind from "./Img/Tailwind.png"
+import python from "./Img/Python.png"
+import bootstrap from "./Img/Bootstrap.png"
 export const Data =[
     {
         id: 1,
         name: "Html",
-        img: "https://th.bing.com/th/id/R.a6a70fb2455d4eb26a7e167e678f40b5?rik=KYyIadxz7MQdtw&pid=ImgRaw&r=0",
+        img: `${html}`,
         desc: {
             desc1: "Etiquetas",
             desc2: "opcion 2",
@@ -12,9 +18,9 @@ export const Data =[
 
     },
     {
-        id: 1,
-        name: "Html",
-        img: "#",
+        id: 2,
+        name: "CSS",
+        img: `${css}`,
         desc: {
             desc1: "Etiquetas",
             desc2: "opcion 2",
@@ -23,9 +29,9 @@ export const Data =[
 
     },
     {
-        id: 1,
-        name: "Html",
-        img: "#",
+        id: 3,
+        name: "JavaScript",
+        img: `${js}`,
         desc: {
             desc1: "Etiquetas",
             desc2: "opcion 2",
@@ -34,9 +40,42 @@ export const Data =[
 
     },
     {
-        id: 1,
-        name: "Html",
-        img: "#",
+        id: 4,
+        name: "React",
+        img: `${react}`,
+        desc: {
+            desc1: "Etiquetas",
+            desc2: "opcion 2",
+            desc3: "opcion 3"
+        }
+
+    },
+    {
+        id: 5,
+        name: "Tailwind",
+        img: `${tailwind}`,
+        desc: {
+            desc1: "Etiquetas",
+            desc2: "opcion 2",
+            desc3: "opcion 3"
+        }
+
+    },
+    {
+        id: 6,
+        name: "Python",
+        img: `${python}`,
+        desc: {
+            desc1: "Etiquetas",
+            desc2: "opcion 2",
+            desc3: "opcion 3"
+        }
+
+    },
+    {
+        id: 7,
+        name: "Bootstrap",
+        img: `${bootstrap}`,
         desc: {
             desc1: "Etiquetas",
             desc2: "opcion 2",
@@ -44,5 +83,6 @@ export const Data =[
         }
 
     }
+
 
 ]
