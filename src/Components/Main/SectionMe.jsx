f94dbfd1f4d8a614e5ece7a15135a81bf4a41aae
Noah <noenavarrete71@outlook.com>
2023-03-15 11:38:25 -0300

    responsive

--- src/Components/Main/SectionMe.jsx
+++ src/Components/Main/SectionMe.jsx
@@ -3,10 +3,10 @@ import Me from "./Img/Me.jpg";
 
 function SectionME() {
     return(
-        <div id="About" className="flex h-screen bg-green-200 gap-10 justify-center pt-12 ">
-           <img src={Me} alt="Me" className="h-[700px]" />
-           <div className="bg-red-200 h-full pt- w-1/3 box-border">
-            <h1>Hi, I´m Noé Navarrete</h1>
+        <div id="About" className="flex flex-wrap h-screen bg-green-200 gap-10 justify-center pt-12 w-screen">
+           <img src={Me} alt="Me" className="max-h-[700px] min-h-[100px] " />
+           <div className="bg-red-200 h-auto max-w-lg sm:pt-48 max- box-border">
+            <h1 className="pb-2">Hi, I´m Noé Navarrete</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, ea odit? Nam doloremque porro voluptates asperiores ipsam ducimus modi reprehenderit illum inventore exercitationem dignissimos, atque incidunt, est dolor. Dolorum, atque?
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nemo delectus, perspiciatis iste similique nam! Ducimus maiores esse molestias! Cum iure impedit enim aliquam nulla quo repudiandae laudantium fugit maxime?
             </p>
