d07c15d9016c12e210b98db68537f5657852428c
Noah <noenavarrete71@outlook.com>
2023-03-15 10:47:51 -0300

    add Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import NavBar from "./Components/Navbar/navbar";
 import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
 import "./App.css"
 function App() {
   return (
       <div>
         <NavBar/>
         <Main/>
+        <Footer/>
+        
       </div>    
       
   
