ffc2696fab59db9df29c91414332a9d03d8ce885
Noah <noenavarrete71@outlook.com>
2023-03-15 13:51:40 -0300

    estilos

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,24 +1,25 @@
 import React from "react";
+import * as styles from "./styles"
 
 function NavBar() {
     return(
-        <nav className="flex w-screem h-16  bg-green-800 justify-between text-white">
-            <div className="flex bg-blue-800 items-center w-1/6 justify-evenly">
-                <a className="" href="">Inicio</a>
+        <nav className="fixed flex w-screen h-16 bg-indigo-800 justify-between text-white shadow-lg shadow-black ">
+            <div className="flex items-center w-1/6 justify-evenly">
+                <a className={styles.a} href="">Inicio</a>
                 </div>
-            <div className="bg-red-400 sm:w-1/3 ">
-                <ul className="flex bg-slate-800 w-full h-full justify-evenly items-center">
-                    <li className="pr-5 shadow-red-200 drop-shadow-2xl">
-                        <a href="">About</a>
+            <div className=" hidden sm:flex sm:w-1/3  ">
+                <ul className="flex  w-full h-full justify-evenly items-center">
+                    <li className={styles.li}>
+                        <a href="" className={styles.a}>About</a>
                         </li>
-                    <li className="pr-5" >
-                        <a href="">Tecnoligias</a>
+                    <li className={styles.li} >
+                        <a href="" className={styles.a}>Tecnoligies</a>
                         </li>
-                    <li className="pr-5">
-                        <a href="">Projects</a>
+                    <li className={styles.li}>
+                        <a href="" className={styles.a}>Projects</a>
                         </li>
-                    <li className="pr-5">
-                        <a href="">Contact</a>
+                    <li className={styles.li}>
+                        <a href="" className={styles.a}>Contact</a>
                         </li>
                 </ul>
             </div>
