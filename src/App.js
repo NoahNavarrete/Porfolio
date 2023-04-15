86d3b44561a460e3324f69580e28a1d33be0fa71
Noah <noenavarrete71@outlook.com>
2023-04-15 12:44:52 -0300

    Se agrega react-scroll links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import React from "react";
 import NavBar from "./Components/Navbar/navbar";
-import Main from "./Components/Main/Main";
-import Footer from "./Components/Footer/Footer";
+import Main from "./Components/Main/Main"
+import Footer from "./Components/Footer/Footer"
 import "./App.css"
 function App() {
   return (
-      <div>
-        <NavBar/>
-        <Main/>
-        <Footer/>
-        
-      </div>    
       
-  
+     <div>
+      <NavBar/>
+      <Main/>
+      <Footer/>
+      
 
+     </div>
+     
+      
     
   );
 }
--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,25 +1,49 @@
 import React from "react";
 import * as styles from "./styles"
+import { Link } from "react-scroll";
+
+
+
 
 function NavBar() {
     return(
         <nav className="fixed flex w-screen h-16 bg-indigo-800 justify-between text-white shadow-lg shadow-black ">
             <div className="flex items-center w-1/6 justify-evenly">
-                <a className={styles.a} href="">Inicio</a>
+                <Link to={"About"} href="#About" className={styles.a} 
+                 smooth={true}
+                 offset={0}
+                 duration={500}
+                 >Inicio</Link>
                 </div>
             <div className=" hidden sm:flex sm:w-1/3  ">
                 <ul className="flex  w-full h-full justify-evenly items-center">
                     <li className={styles.li}>
-                        <a href="" className={styles.a}>About</a>
+                        <Link to={"About"} href="#About" className={styles.a}
+                        smooth={true}
+                        offset={0}
+                        duration={500}
+                        >About</Link>
                         </li>
                     <li className={styles.li} >
-                        <a href="" className={styles.a}>Tecnoligies</a>
+                        <Link to={"Tech"} href="#Tech" className={styles.a}
+                        smooth={true}
+                        offset={0}
+                        duration={500}
+                        >Tecnoligies</Link>
                         </li>
                     <li className={styles.li}>
-                        <a href="" className={styles.a}>Projects</a>
+                        <Link to={"Projects"} href="#Projects" className={styles.a}
+                        smooth={true}
+                        offset={0}
+                        duration={500}
+                        >Projects</Link>
                         </li>
                     <li className={styles.li}>
-                        <a href="" className={styles.a}>Contact</a>
+                        <Link to={"ContactMe"} href="#ContactMe" className={styles.a}
+                        smooth={true}
+                        offset={0}
+                        duration={500}
+                        >Contact</Link>
                         </li>
                 </ul>
             </div>
