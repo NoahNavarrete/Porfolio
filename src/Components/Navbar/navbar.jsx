2163b3ef90892f667e58f6263a12cfc337c63517
Noah <noenavarrete71@outlook.com>
2023-04-18 21:44:24 -0300

    navbar styles

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import * as styles from "./styles"
 import { Link } from "react-scroll";
+import {faBars } from "@fortawesome/free-solid-svg-icons"
+import {Icon }from "../Footer/Icon"
 
 
 
 
 function NavBar() {
+    const [open, setOpen] = useState(true)
+
+
+
     return(
-        <nav className="fixed flex w-screen h-16 bg-indigo-800 justify-between text-white shadow-lg shadow-black ">
+        <nav className="fixed z-50 sm:justify-between flex w-screen h-16 bg-sky-600 justify-between text-white shadow-lg shadow-black ">
             <div className="flex items-center w-1/6 justify-evenly">
                 <Link to={"About"} href="#About" className={styles.a} 
                  smooth={true}
@@ -15,8 +21,14 @@ function NavBar() {
                  duration={500}
                  >Inicio</Link>
                 </div>
-            <div className=" hidden sm:flex sm:w-1/3  ">
-                <ul className="flex  w-full h-full justify-evenly items-center">
+                <div className="sm:hidden pr-4 flex self-center ">
+                    <button className="" onClick={()=> setOpen(!open)}>
+                    <Icon icon={faBars} css="sm:hidden text-4xl"/>
+
+                    </button>
+                </div>
+            <div className={`${!open ? "bg-sky-600 mt-16 flex flex-col absolute w-full " : " hidden sm:visible"}  sm:w-1/3 sm:flex  `}>
+                <ul className={`${!open ? "flex flex-col self-center" : "w-0"} sm:visibles sm:flex sm:w-full sm:h-full sm:pr-40 sm:justify-evenly sm:items-center`}>
                     <li className={styles.li}>
                         <Link to={"About"} href="#About" className={styles.a}
                         smooth={true}
@@ -34,14 +46,14 @@ function NavBar() {
                     <li className={styles.li}>
                         <Link to={"Projects"} href="#Projects" className={styles.a}
                         smooth={true}
-                        offset={0}
+                        offset={-200}
                         duration={500}
                         >Projects</Link>
                         </li>
                     <li className={styles.li}>
                         <Link to={"ContactMe"} href="#ContactMe" className={styles.a}
                         smooth={true}
-                        offset={0}
+                        offset={-100}
                         duration={500}
                         >Contact</Link>
                         </li>
